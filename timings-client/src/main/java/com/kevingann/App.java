ef3fbed5647eedc51be1874491e1152b82bce847
Kevin Gann <kevin.gann@rev.com>
2017-10-26 12:39:10 -0500

    Creating initial project.

---
+++ timings-client/src/main/java/com/kevingann/App.java
@@ -0,0 +1,13 @@
+package com.kevingann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
